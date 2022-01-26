2d77412b0b40f64bb95b58fdc958deebed509424
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2022-01-26 13:38:49 +0100

    maj de récupération dossier

--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 10 janvier 2022 </p>';
+echo '<p>update : 26 janvier 2022 </p>';
 ?>
-
