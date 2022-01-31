2c34778ff7af89645691937f80a4bc7ac5173a39
ThomPlan173 <thomas.plantefeve@outlook.fr>
2022-01-31 09:53:48 +0100

    test fonctionnel 31/01/22

--- src/index.php
+++ src/index.php
@@ -3,5 +3,5 @@ echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 31 janvier 2022 </p>';
 echo "bonjour"."<br>";
-echo "Je teste si cela fonctionne"
+echo "Je teste si cela fonctionne, mais ça a l'air" ."<br>";
 ?>
