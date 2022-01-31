1bb80433830063d119e913b83331f79019b569e8
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2022-01-31 09:36:30 +0100

    maj 31/01

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 26 janvier 2022 </p>';
+echo '<p>update : 31 janvier 2022 </p>';
+echo "bonjour";
 ?>
